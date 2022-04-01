96a84e2212ce9ef428a6d5ba54e21eac979eb9d7
lhvan2 <user.name>
2022-04-01 11:07:52 +0700

    test create function

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from 'aws-lambda';
-import * as _ from 'lodash';
-
 export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
   const message = "Im full-stack developer"
   const response = {
